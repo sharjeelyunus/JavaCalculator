c91ff1e0da5d53865819b65b1789290ef4eb539c
SharjeelDev <61178058+SharjeelDev@users.noreply.github.com>
2020-03-20 01:45:43 +0500

    Initial commit

---
+++ src/JavaCalculator.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+
+public class JavaCalculator {
+    private JPanel JavaCalculator;
+    private JTextField textField1;
+    private JButton button6;
+    private JButton button7;
+    private JButton button8;
+    private JButton button9;
+    private JButton button10;
+    private JButton button11;
+    private JButton button12;
+    private JButton button13;
+    private JButton button14;
+    private JButton button15;
+    private JButton button16;
+    private JButton button17;
+    private JButton button18;
+    private JButton button19;
+    private JButton button20;
+    private JButton button21;
+    private JButton button22;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("JavaCalculator");
+        frame.setContentPane(new JavaCalculator().JavaCalculator);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
